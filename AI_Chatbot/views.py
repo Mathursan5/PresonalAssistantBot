b863ec71515776ac8bd81098cb18c468d16c3f0e
mathu <mathursanmathu13@gamil.com>
2024-05-19 00:45:21 +0530

    Html, CSS & Jquery Added

--- AI_Chatbot/views.py
+++ AI_Chatbot/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request,'AI_Chatbot/index.html')
+
+def specific(request):
+    return HttpResponse(list)
+
+# def article(requeset,article_id):
+#     return render(requeset,'AI_Chatbot/index.html',{article_id:article_id})
--- PersonalAssistantBot/settings.py
+++ PersonalAssistantBot/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'AI_Chatbot.apps.AiChatbotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
